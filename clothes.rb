39c9f3c2ab5b5e1839c3c5a2560a7a41f7684d60
Adam Dittebrand <Adam Dittebrand>
2016-08-22 20:50:36 -0400

    LRTHW ex. 1-7

---
+++ clothes.rb
@@ -0,0 +1,20 @@
+puts "I will now count my clothes"
+
+puts "T-shirts #{14}"
+puts "Polos #{6}"
+puts "Button-ups #{4}"
+
+puts "Jeans #{3}"
+puts "Shorts #{3}"
+puts "Khakis #{2}"
+puts "Suit #{2}"
+
+puts "Sneakers #{6}"
+puts "Wingtip #{1}"
+puts "Boots #{1}"
+
+puts "Total shirts #{14+6+4}"
+puts "Total pants #{3+3+2+2}"
+puts "Total shoes #{6+1+1}"
+
+puts "Is it greater? #{14+6+4>3+3+2+2}"
---
+++ ex6.rb
@@ -0,0 +1,25 @@
+types_of_people = 10
+# this line establishes a value for x
+x = "There are #{types_of_people} types of people."
+# this line establishes a value for binary
+binary = "binary"
+# this line establishes a value for do_not
+do_not = "don't"
+y = "Those who know #{binary} and those who #{do_not}."
+
+puts x
+puts y
+
+puts "I said: #{x}."
+puts "I also said: '#{y}'."
+
+hilarious = false
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+#this line puts the joke evaluation into the code
+puts joke_evaluation
+
+w = "This is the left side of..."
+e = "a string with a right side."
+
+puts w + e
